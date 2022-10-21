8bd9188d2613d31d81d15c36f4b88dd2ed850cf4
Sergeyego <sergen1@rambler.ru>
2022-10-21 16:45:51 +0300

    inital

---
+++ app.js
@@ -0,0 +1,36 @@
+const docx = require('docx');
+const app = require('express')()
+
+const host = '127.0.0.1'
+const port = 7000
+
+app.get('/home', async (req, res) => {
+  res.status(200).type('text/plain')
+  res.send('Home page')
+})
+
+app.get('/about', (req, res) => {
+  res.status(200).type('text/plain')
+  res.send('About page')
+})
+
+/*app.post('/api/admin', (req, res) => {
+  res.status(200).type('text/plain')
+  res.send('Create admin request')
+})
+
+app.post('/api/user', (req, res) => {
+  res.status(200).type('text/plain')
+  res.send('Create user request')
+})*/
+
+require('./routes/test.js')(app);
+
+app.use((req, res, next) => {
+  res.status(404).type('text/plain')
+  res.send('Not found')
+})
+
+app.listen(port, host, function () {
+  console.log(`Server listens http://${host}:${port}`)
+})
