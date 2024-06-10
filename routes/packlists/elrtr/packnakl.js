7c8d8ca16726498a54e26954a645d1aa390c1f2d
sergen <sergen1@rambler.ru>
2024-06-10 12:41:48 +0300

    накладная упаковки по бригадам

--- routes/packlists/elrtr/packnakl.js
+++ routes/packlists/elrtr/packnakl.js
@@ -1,7 +1,7 @@
 const db = require('../../../postgres.js');
 const locale = require('../../../locale.js');
 
-let getTable = function(data){
+let getTable = function(data, by_rab){
     let sum = 0.0;
     let part_sum = 0.0;
     let tbl='<table class="tablestyle" border="1" cellspacing="0" cellpadding="2">';
@@ -29,11 +29,12 @@ let getTable = function(data){
         let is_end = (i==data.length-1);
         let print_itogo = is_end;
         if (!is_end) {
-            print_itogo = (data[i].part!=data[i+1].part);
+            print_itogo = by_rab ? (data[i].rab!=data[i+1].rab) : (data[i].part!=data[i+1].part);
         }
         if (print_itogo){
+            let str = by_rab ? (data[i].rab) : (data[i].marka+' п. '+data[i].part);
             tbl+='<tr>'+
-            '<td class="leftalign" colspan="5"><b>'+data[i].marka+' п. '+data[i].part+' итого</b></td>'+
+            '<td class="leftalign" colspan="5"><b>'+str+' итого</b></td>'+
             '<td class="rightalign"><b>'+locale.insNumber(part_sum,1)+'</b></td>'+
             '</tr>';
             part_sum=0.0;
@@ -48,13 +49,15 @@ let getTable = function(data){
 }
 
 module.exports = function (app) {
-    app.use("/packnakl/elrtr/:dat/:id_src/:id_master/", async (req, res) => {
+    app.get("/packnakl/elrtr/:dat/:id_src/:id_master/", async (req, res) => {
+        let by_rab = !(typeof req.query.by_rab=="undefined" || req.query.by_rab=="false");
         db.one("select i.nakl_nam as oper, "+
             "(select rr.snam from kamin_empl rr where rr.id = $1 ) as from, "+
             "(select ne.nam  from nakl_emp ne where ne.id = 1) as to "+
             "from istoch i where i.id = $2"
             ,[ String(req.params["id_master"]), Number(req.params["id_src"]) ])
             .then((dataTitle) => {
+                let sort = by_rab ? "rr.snam, e.marka, p.diam, date_part('year',p.dat_part), p.n_s, p2.nam" : "e.marka, p.diam, date_part('year',p.dat_part), p.n_s, rr.snam, p2.nam";
                 db.any("select e.marka || ' ф ' || cast(p.diam as varchar(3)) as marka, "+
                     "p.n_s||'-'||date_part('year',p.dat_part) as part, rr.snam as rab, p2.nam as pal, epo.kvo as kvo "+
                     "from el_pallet_op epo "+
@@ -74,7 +77,7 @@ module.exports = function (app) {
                             oper: dataTitle.oper,
                             to: dataTitle.to,
                             from: dataTitle.from,
-                            table: getTable(data),
+                            table: getTable(data,by_rab),
                             passed: dataTitle.from,
                             accepted: dataTitle.to
                         })
